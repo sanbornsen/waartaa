6742242de7cc4bb3018250494eaeadad1c64e1af
Sudipta Sen <sanborn.sen@gmail.com>
2016-06-14 01:08:34 +0530

    Code refractor

---
+++ waartaa/client/app/containers/chatapp/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChatMenu from './chatmenu';
+import ChatRoom from './chatroom';
+
+var ChatApp = React.createClass({
+	render: function() {
+		return <div>
+			<ChatMenu />
+			<ChatRoom />
+		</div>
+	}
+})
+
+module.exports = ChatApp;
